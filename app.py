aafd6e793c59e4f451ec2a9e4b81097a0793a8d3
Rohit Singh <imrohitsingh4105@gmail.com>
2026-06-17 13:21:52 +0530

    Complete homepage design

--- app.py
+++ app.py
@@ -24,6 +24,17 @@ def show_player(player_id):
 
     return render_template('player_info.html',player=req_player)
 
+@app.route("/build-team")
+def build_team():
+    return render_template('build_team.html')
+
+@app.route("/hof-team")
+def hall_of_fame():
+    return render_template('hof_team.html')
+
+@app.route("/mix-team")
+def mix_team():
+    return render_template('mix_team.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
